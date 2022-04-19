572f6e7c1a25215f0aef8fad6b788d7a636b03cd
Nita Constantin <nita.constantin880722@yahoo.com>
2022-04-19 18:08:03 +0300

    curs 1

---
+++ hello/hello.js
@@ -0,0 +1,19 @@
+// ruleaza tellMe dupa 3000ms(3secunde)
+setTimeout(tellMe, 3000)
+
+// creaza o noua instructiune(functie) numita tellMe
+function tellMe(){   
+    alert('Hello!')
+    alert('Welcome to JavaScript!')
+}
+//alertMeTomorrow => camel case alert_me_tomorrow => snake case; => comentariu pe o linie
+/* 
+comentariu
+    pe mai multe randuri
+    in javascript
+*/
+
+
+var boolean = true/false
+var string = 'String'
+var number = 10/10.5
---
+++ variabile/variabile.js
@@ -0,0 +1,14 @@
+var date = new Date()
+var hour = date.getHours()
+var minutes = date.getMinutes()
+var seconds = date.getSeconds()
+var month = date.getMonth() + 1
+var day = date.getDate()
+//var hour = 20
+var isRaining = false
+var message = "Hello! Suntem in " + day + ":" + month + " Iar ora este " + hour + ":" + minutes +  ':' + seconds + "!"
+// o variabila fara valoare (udefined)
+var city
+
+//alert(message)
+alert(message)
